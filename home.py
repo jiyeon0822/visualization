60e9cebbb79926b901f4aaa7928e5727c49a77da
jiyeon0822 <jiyeon910822@gmail.com>
2023-10-10 09:50:40 +0000

    commit' git status

--- home.py
+++ home.py
@@ -1,19 +1,4 @@
 import streamlit as st
 
-import pages.sample
-import pages.about
-
-PAGES = {
-  "home" : pages.sample,
-  "about" : pages.about
-}
-
-
-def main():
-
-  st.sidebar.markdown('### Contents')
-  selection = st.sidebar.radio("select", list(PAGES.keys()))
-  page = PAGES[selection]
-  st.write(page)
-  
-  
+st.markdown("# jiyeon's home 🎈")
+st.sidebar.markdown("# jiyeon's home 🎈")
