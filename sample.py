30bd66286e8060eaacb9cb30895f0290230dc44b
jiyeon0822 <jiyeon910822@gmail.com>
2023-10-10 17:50:29 +0900

    Update sample.py
    sample

--- sample.py
+++ sample.py
@@ -1,3 +1,25 @@
 import streamlit as st
+import awesome_streamlit as ast
 
-st.write('Hello world')
+import src.home
+import src.about
+
+
+
+
+PAGES = {
+  "home" : src.home,
+  "about" : src.about
+}
+
+
+def main():
+
+  st.sidebar.markdown('### Contents')
+  selection = st.sidebar.radio("select", list(PAGES.keys()))
+  page = PAGES[selection]
+  
+  with st.spinner(f"Loading {selection} ..."):
+    ast.shared.components.write_page(page)
+  
+  
